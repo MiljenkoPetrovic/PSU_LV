003bb70c6a25b46264c315a328beb09829c75d0a
MiljenkoPetrovic <MiljenkoMimoPetrovic@gmail.com>
2021-03-18 04:53:52 +0100

    Zadatak 6 gotov

--- LV1/Zadatak6.py
+++ LV1/Zadatak6.py
@@ -1,7 +1,8 @@
 import re
 email = re.compile('\w+@\w+\.[a-z]{3}')
-
+dictionary={}
 mailovi=[]
+i=0
 string="From:" 
 try:
     fhand = open("LV1\mbox-short.txt")
@@ -15,10 +16,27 @@ for line in fhand:
     if line.startswith(string) and "From:" in line:
             mailovi.extend(email.findall(line))
             domain = line.split('@')[1]
-            print(domain)
-
+            if domain not in dictionary:
+                dictionary[domain]=1
+            elif domain in dictionary:
+                dictionary[domain]+=1
         
-#print(mailovi)
+
+res=[]
+for i in mailovi: 
+    if i not in res: 
+        res.append(i) 
+
+
+
+
+for i in range(5):
+    print(res[i])
+    
+
+for x, y in dictionary.items():
+    print(x, ":", y)
+
 
 
 
